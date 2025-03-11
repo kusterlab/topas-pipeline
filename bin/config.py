cbbdffd09138d0ac72c303704e826f4bda524786
Matthew The <matthew.the@tum.de>
2025-03-11 15:15:39 +0100

    add json file with defaults for config

--- bin/config.py
+++ bin/config.py
@@ -1,27 +1,24 @@
 import json
 
+from pathlib import Path
+
 
 def load(config_file):
+    with open(Path(__file__).parent.parent / "config_defaults.json", "r") as inp:
+        config_defaults = json.load(inp)
+
     with open(config_file, "r") as inp:
         configs = json.load(inp)
-
-    # TODO: replace this with a default configuration file
-    if "data_types" not in configs:
-        configs["data_types"] = ["fp", "pp"]
-
-    if "run_lfq" not in configs["preprocessing"]:
-        configs["preprocessing"]["run_lfq"] = False
     
-    if "normalize_to_reference" not in configs["preprocessing"]:
-        configs["preprocessing"]["normalize_to_reference"] = False
-
-    if "num_threads" not in configs["simsi"]:
-        configs["simsi"]["num_threads"] = 4
-
-    if "portal" not in configs:
-        configs["portal"] = {"update": 0}
-
-    if "slack_webhook_url" not in configs:
-        configs["slack_webhook_url"] = ""
+    configs = merge_dicts(config_defaults, configs)
 
     return configs
+
+
+def merge_dicts(defaults, custom):
+    for key, value in custom.items():
+        if isinstance(value, dict) and key in defaults and isinstance(defaults[key], dict):
+            defaults[key] = merge_dicts(defaults[key], value)  # Recursive merge
+        else:
+            defaults[key] = value  # Overwrite default value
+    return defaults
