8fc1a3a86a1c244f0da9ffbf140395bf951b69fa
Matthew The <matthew.the@tum.de>
2025-10-15 20:42:54 +0200

    Added bridge channel normalization

---
+++ topas_pipeline/normalization.py
@@ -0,0 +1,244 @@
+import sys
+import logging
+from pathlib import Path
+
+import pandas as pd
+import numpy as np
+
+from tqdm import tqdm
+
+tqdm.pandas()
+
+import warnings
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
+warnings.filterwarnings(action="ignore", category=RuntimeWarning)
+
+from . import sample_annotation
+from . import preprocess_tools as prep
+
+# hacky way to get the package logger instead of just __main__ when running as a module
+logger = logging.getLogger(__package__ + "." + Path(__file__).stem)
+
+
+def apply_bridge_channel_normalization(
+    results_folder: str,
+    sample_annotation_df: pd.DataFrame,
+    min_occurrence: float = 2 / 3,  # Good Value for phospho
+):
+    logger.info("Reading preprocessed_pp2_agg.csv")
+    pp_df = pd.read_csv(f"{results_folder}/preprocessed_pp2_agg.csv")
+
+    phospho_df = (
+        pp_df.filter(regex=r"^(?!Identification metadata)")
+        .drop(
+            columns=[
+                "Delocalized sequence",
+                "Modified sequence representative",
+                "Modified sequence representative degree",
+                "Proteins",
+            ]
+        )
+        .set_index(["Modified sequence group", "Gene names"])
+    )
+
+    sample_qc_lot_mapping_df = get_sample_qc_lot_mapping_df(
+        phospho_df.columns, sample_annotation_df
+    )
+
+    ref_cols = sample_qc_lot_mapping_df.loc[
+        sample_qc_lot_mapping_df["QC Lot"].notna()
+    ].index
+    pat_cols = sample_qc_lot_mapping_df.loc[
+        sample_qc_lot_mapping_df["QC Lot"].isna()
+    ].index
+
+    phospho_df_corrected, correction_factors = within_qc_lot_normalization(
+        phospho_df, ref_cols, min_occurrence, sample_qc_lot_mapping_df
+    )
+    correction_factors.to_csv(f"{results_folder}/peptide_correction_factors.csv")
+
+    logger.info("Applying across QC lot normalization")
+
+    phospho_df_ref_corrected = phospho_df_corrected.loc[:, ref_cols]
+    phospho_df_corrected = phospho_df_corrected.loc[:, pat_cols]
+
+    # temporarily center patient data around 0 for each peptide
+    avg_intensity = phospho_df_corrected.median(axis=1)
+    phospho_df_corrected2 = (phospho_df_corrected.T - avg_intensity).T
+    phospho_df_ref_corrected2 = (phospho_df_ref_corrected.T - avg_intensity).T
+
+    # correct QC lots with at least 100 patient samples
+    qc_lots_to_correct = (
+        sample_qc_lot_mapping_df["QC Lot group"]
+        .value_counts()[sample_qc_lot_mapping_df["QC Lot group"].value_counts() >= 100]
+        .index
+    )
+    for qc_lot_group in qc_lots_to_correct:
+        qc_lot_samples = sample_qc_lot_mapping_df["QC Lot group"] == qc_lot_group
+        qc_lot_patient_samples = sample_qc_lot_mapping_df.loc[
+            qc_lot_samples & sample_qc_lot_mapping_df["QC Lot"].isna()
+        ].index
+        qc_lot_ref_samples = sample_qc_lot_mapping_df.loc[
+            qc_lot_samples & sample_qc_lot_mapping_df["QC Lot"].notna()
+        ].index
+
+        avg_patient_intensity = phospho_df_corrected2.loc[
+            :, qc_lot_patient_samples
+        ].median(axis=1)
+        phospho_df_corrected2.loc[:, qc_lot_patient_samples] = (
+            phospho_df_corrected2.loc[:, qc_lot_patient_samples].sub(
+                avg_patient_intensity, axis=0
+            )
+        )
+        phospho_df_ref_corrected2.loc[:, qc_lot_ref_samples] = (
+            phospho_df_ref_corrected2.loc[:, qc_lot_ref_samples].sub(
+                avg_patient_intensity, axis=0
+            )
+        )
+
+    # bring back to original intensity level
+    phospho_df_corrected2 = (phospho_df_corrected2.T + avg_intensity).T
+    phospho_df_ref_corrected2 = (phospho_df_ref_corrected2.T + avg_intensity).T
+
+    # map column names to patient names
+    channel_to_sample_id_dict = sample_annotation.get_channel_to_sample_id_dict(
+        sample_annotation_df,
+        remove_qc_failed=True,
+        remove_replicates=False,
+    )
+
+    index_cols = ["Modified sequence group", "Gene names"]
+    phospho_df_corrected2 = prep.rename_columns_with_sample_ids(
+        phospho_df_corrected2.reset_index(),
+        channel_to_sample_id_dict,
+        index_cols=index_cols,
+    )
+
+    phospho_df_ref_corrected2 = prep.rename_columns_with_sample_ids(
+        phospho_df_ref_corrected2.reset_index(),
+        channel_to_sample_id_dict,
+        index_cols=index_cols,
+    )
+
+    logger.info("Writing results")
+    phospho_df_corrected2.to_csv(
+        f"{results_folder}/preprocessed_pp2_agg_batchcorrected.csv",
+        float_format="%.6f",
+        index=False,
+    )
+    phospho_df_ref_corrected2.to_csv(
+        f"{results_folder}/preprocessed_pp2_agg_batchcorrected_refonly.csv",
+        float_format="%.6f",
+        index=False,
+    )
+
+
+def get_sample_qc_lot_mapping_df(
+    sample_columns: pd.Index, sample_annotation_df: pd.DataFrame
+) -> pd.DataFrame:
+    sample_mapping_df = sample_columns.to_frame().reset_index()
+    sample_mapping_df["batch"] = sample_mapping_df["index"].str.split("_Batch").str[-1]
+    sample_mapping_df["channel"] = (
+        sample_mapping_df["index"].str.split(" ").str[-2].astype(int)
+    )
+    sample_mapping_df = sample_mapping_df.merge(
+        sample_annotation_df[["Batch Name", "TMT Channel", "QC Lot"]],
+        left_on=["batch", "channel"],
+        right_on=["Batch Name", "TMT Channel"],
+    )
+    sample_mapping_df["QC Lot group"] = sample_mapping_df.groupby("batch")[
+        "QC Lot"
+    ].transform("mean")
+    sample_mapping_df = sample_mapping_df.drop(columns=[0, "Batch Name", "TMT Channel"])
+    sample_mapping_df = sample_mapping_df.set_index("index")
+    return sample_mapping_df
+
+
+def within_qc_lot_normalization(
+    phospho_df: pd.DataFrame,
+    ref_cols: list[str],
+    min_occurrence: float,
+    sample_qc_lot_mapping_df: pd.DataFrame,
+) -> tuple[pd.DataFrame, pd.Series]:
+    logger.info("Applying within QC lot normalization")
+    high_count_mask = phospho_df[ref_cols].notnull().mean(axis=1) > min_occurrence
+    phospho_df_corrected = phospho_df[high_count_mask].progress_apply(
+        row_wise_normalize, sample_qc_lot_mapping_df=sample_qc_lot_mapping_df, axis=1
+    )
+    correction_factors = phospho_df_corrected - phospho_df[high_count_mask]
+    return phospho_df_corrected, correction_factors
+
+
+def mean_f(x):
+    return np.nanmean(x)
+
+
+def mean_realspace_f(x):
+    return np.log10(np.nanmean(np.power(10, x)))
+
+
+def row_wise_normalize(
+    row: pd.Series, sample_qc_lot_mapping_df: pd.DataFrame
+) -> pd.Series:
+    """
+    return values : "intensity raw", "ref correction", "intensity corrected"
+    """
+    # Grab a single peptide
+    vals = row.rename("intensity raw")
+    vals = pd.concat([vals, sample_qc_lot_mapping_df], axis=1)
+    vals["sample type"] = vals["QC Lot"]
+
+    # Calculates the ref correction for each mix type.
+    # Aggregation happens as mean in the realspace. (Currently slow implementation !! TODO: pull out power and log)
+    # Real space aggregation is more robust against low-intense outliers than mean in log space
+
+    # Calculate the mean ref intensity per Batch and QC Lot
+    mean_intensities = (
+        vals[vals["channel"].isin({10, 11})]
+        .groupby(["sample type", "batch"], as_index=False)["intensity raw"]
+        .agg(mean_realspace_f, engine="cython")
+    )
+
+    # Calculate the mean ref intensity per QC lot
+    mean_intensities["mix mean intensities"] = mean_intensities.groupby("sample type")[
+        "intensity raw"
+    ].transform("median")
+
+    # Calculate correction factor relative to mean of QC lot
+    mean_intensities["ref correction"] = (
+        mean_intensities["mix mean intensities"] - mean_intensities["intensity raw"]
+    )
+
+    # Aggregate multiple mixes into a single batch correction value (necessary if multiple QC lots are present in a single batch)
+    batch_correction = mean_intensities.groupby("batch")["ref correction"].mean()
+    vals = vals.merge(batch_correction, left_on="batch", right_index=True)
+
+    # Correct intensity values
+    vals["intensity corrected"] = vals["intensity raw"] + vals["ref correction"]
+    return vals["intensity corrected"]
+
+
+"""
+python3 -m topas_pipeline.phospho_grouping -c config_patients.json
+"""
+if __name__ == "__main__":
+    import argparse
+
+    from . import config
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c", "--config", required=True, help="Absolute path to configuration file."
+    )
+    args = parser.parse_args(sys.argv[1:])
+
+    configs = config.load(args.config)
+
+    sample_annotation_df = sample_annotation.load_sample_annotation(
+        configs.sample_annotation
+    )
+
+    apply_bridge_channel_normalization(
+        results_folder=configs.results_folder, sample_annotation_df=sample_annotation_df
+    )
--- topas_pipeline/tools/create_sample_annotation.py
+++ topas_pipeline/tools/create_sample_annotation.py
@@ -1,12 +1,12 @@
 """
 
 Description:
-Script to create input sample annotation file for pipeline from metadata file. 
+Script to create input sample annotation file for pipeline from metadata file.
 The location of MQ searches are given by Cohort column info and is either hard coded to be Sarcoma (for patients) or Workflow_Test (for models) based on Batch name.
 Reference channels are added for each batch as TMT channel 10 and 11.
 
 Usage:
-    
+
     python tools/create_sample_annotation.py Metadata_file.xlsx output_folder
 
 """
@@ -14,52 +14,135 @@ Usage:
 import sys
 import re
 import datetime
+import argparse
+
 import pandas as pd
 
 
-
 def main(argv):
-    metadata_file = argv[0]
-    output_folder = argv[1]
-    metadata = pd.read_excel(metadata_file, engine='openpyxl')
-    metadata = metadata[metadata['QC'] != 'exclude']
-    metadata = metadata.rename(columns={'Batch_No': 'Batch Name', 'TMT_Channel': 'TMT Channel'})
+    parser = argparse.ArgumentParser(
+        description="Script to create input sample annotation file for pipeline from metadata file."
+    )
+    parser.add_argument(
+        "--metadata-file", required=True, help="Path to the metadata Excel file"
+    )
+    parser.add_argument(
+        "--output-folder",
+        required=True,
+        help="Path to the folder where to write annotation file",
+    )
+    parser.add_argument(
+        "--qc-lot-mapping-file",
+        help="Path to Excel file with two mandatory columns: 'Batch Name' and 'QC Lot'",
+    )
+
+    args = parser.parse_args(argv)
+
+    metadata_file = args.metadata_file
+    output_folder = args.output_folder
+    qc_lot_mapping_file = args.qc_lot_mapping_file
+
+    # Rest of your logic goes here
+    print(f"Metadata file: {metadata_file}")
+    print(f"Output folder: {output_folder}")
+    print(f"QC lot mapping file: {qc_lot_mapping_file}")
+
+    metadata_df = pd.read_excel(metadata_file, engine="openpyxl")
+    metadata_df = metadata_df[metadata_df["QC"] != "exclude"]
+    metadata_df = metadata_df.rename(
+        columns={"Batch_No": "Batch Name", "TMT_Channel": "TMT Channel"}
+    )
 
     new_ref_rows = []
-
     # Iterate through each unique Batch Name
-    for batch in metadata['Batch Name'].unique():
+    for batch in metadata_df["Batch Name"].unique():
         for channel in [10, 11]:
             # Create a new row as a dictionary
             new_row = {
-                'Batch Name': batch,
-                'Sample name': f"ref_channel_{channel}_batch{batch}",
-                'TMT Channel': channel,
-                'QC': 'passed',
-                'is_reference': True
+                "Batch Name": batch,
+                "Sample name": f"ref_channel_{channel}_batch{batch}",
+                "TMT Channel": channel,
+                "QC": "passed",
+                "is_reference": True,
             }
             # Append the new row to the list
             new_ref_rows.append(new_row)
 
     # Convert the list of new rows into a DataFrame
-    new_rows_df = pd.DataFrame(new_ref_rows)
+    ref_channel_df = pd.DataFrame(new_ref_rows)
+
+    if qc_lot_mapping_file:
+        qc_lot_mapping_df = pd.read_excel(qc_lot_mapping_file, dtype=str)
+
+        qc_lot_mapping_df = pd.DataFrame(
+            [x for _, row in qc_lot_mapping_df.iterrows() for x in expand_rows(row)]
+        )
+        duplicates = qc_lot_mapping_df[
+            qc_lot_mapping_df.duplicated(
+                subset=["Batch Name", "TMT Channel"], keep=False
+            )
+        ]
+        if len(duplicates) > 0:
+            raise ValueError(
+                f"Found duplicate entries in QC lot mapping file:\n{duplicates}"
+            )
+
+        ref_channel_df = ref_channel_df.merge(
+            qc_lot_mapping_df,
+            on=["Batch Name", "TMT Channel"],
+            how="left",
+            validate="1:1",
+        )
+
+        if ref_channel_df["QC Lot"].hasnans:
+            raise ValueError(
+                f"Some QC channels did not have QC Lots:\n{ref_channel_df[ref_channel_df['QC Lot'].isna()]}"
+            )
 
     # Concatenate the new rows DataFrame with the original DataFrame
-    metadata = pd.concat([metadata, new_rows_df], ignore_index=True)
+    metadata_df = pd.concat([metadata_df, ref_channel_df], ignore_index=True)
 
-    metadata['Cohort'] = 'Sarcoma'
+    metadata_df["Cohort"] = "Sarcoma"
 
     def update_cohort(row):
-        batch_name = str(row['Batch Name'])  # Convert the value to string
+        batch_name = str(row["Batch Name"])  # Convert the value to string
         # Check if the batch name contains both letters and numbers
-        if re.search(r'[A-Za-z]', batch_name) and re.search(r'\d', batch_name):
+        if re.search(r"[A-Za-z]", batch_name) and re.search(r"\d", batch_name):
             return "Workflow_Test"
-        return row['Cohort']
+        return row["Cohort"]
 
-    metadata['Cohort'] = metadata.apply(update_cohort, axis=1)
+    metadata_df["Cohort"] = metadata_df.apply(update_cohort, axis=1)
+
+    metadata_df = metadata_df.sort_values(['Batch Name', 'TMT Channel'])
+
     today = datetime.datetime.today().date()
-    metadata.to_csv(f'{output_folder}/sample_annotation_{today}.csv', index=False)
+    metadata_df.to_csv(f"{output_folder}/sample_annotation_{today}.csv", index=False)
+
+    print(f"Written sample annotation file to: {output_folder}/sample_annotation_{today}.csv")
 
 
-if __name__ == '__main__':
+def expand_rows(row: pd.Series):
+    batch = row["Batch Name"]
+    qc = row["QC Lot"]
+
+    # Expand batch range or leave as string
+    if "-" in batch:
+        start, end = batch.split("-")
+        batches = list(range(int(start), int(end) + 1))
+    else:
+        batches = [batch]  # keep string like 'CL15'
+
+    # If mixed, create per-channel rows
+    mixed_match = re.findall(r"channel (\d+)=(\d+)", qc)
+    for b in batches:
+        if mixed_match:
+            for channel, lot in mixed_match:
+                yield {"Batch Name": b, "TMT Channel": int(channel), "QC Lot": int(lot)}
+        else:
+            # Same QC for all channels 10 and 11
+            for ch in [10, 11]:
+                yield {"Batch Name": b, "TMT Channel": ch, "QC Lot": int(qc)}
+
+
+if __name__ == "__main__":
     main(sys.argv[1:])
