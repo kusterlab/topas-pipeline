b19f8537568abd272436f62a04dc8c94ab7d3742
Vishal Sukumar <vishal.sukumar@tum.de>
2026-07-07 18:36:29 +0200

    formatted

--- topas_pipeline/io/reader.py
+++ topas_pipeline/io/reader.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 import pandas as pd
 import pyarrow.parquet as pq
 
+
 class Reader(ABC):
     def __init__(self, file_path: str):
         self.file_path = file_path
@@ -25,15 +26,19 @@ class TxtReader(Reader):
         self.usecols = usecols if usecols else None
         sep = self.kwargs.get("sep", "\t")
         return pd.read_csv(self.file_path, usecols=usecols, sep=sep)
-    
+
     def get_header(self):
         """Get the header of the CSV file"""
         if self.usecols:
             return self.usecols
-        return pd.read_csv(self.file_path, nrows=0, sep=self.kwargs.get("sep", "\t")).columns.tolist()
+        return pd.read_csv(
+            self.file_path, nrows=0, sep=self.kwargs.get("sep", "\t")
+        ).columns.tolist()
+
 
 class ParquetReader(Reader):
     """Reader for parquet files"""
+
     def __init__(self, file_path: str, **kwargs):
         super().__init__(file_path)
         self.kwargs = kwargs
@@ -43,19 +48,20 @@ class ParquetReader(Reader):
         self.usecols = usecols if usecols else None
         df = pq.read_table(self.file_path, columns=usecols).to_pandas()
         return df
-    
+
     def get_header(self):
         """Get the header of the parquet file"""
         if self.usecols:
             return self.usecols
         return pq.read_metadata(self.file_path).schema.names
 
+
 class ReaderFactory:
     _readers = {
         "csv": (TxtReader, {"sep": ","}),
         "tsv": (TxtReader, {"sep": "\t"}),
         "txt": (TxtReader, {"sep": "\t"}),
-        "parquet": (ParquetReader, {})
+        "parquet": (ParquetReader, {}),
     }
 
     @classmethod
@@ -65,4 +71,3 @@ class ReaderFactory:
             raise ValueError(f"Unsupported file type: {file_extension}")
         reader_class, default_kwargs = cls._readers.get(file_extension)
         return reader_class(file_path, **default_kwargs)
-
